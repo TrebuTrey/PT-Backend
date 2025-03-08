c40a5d42f45d5674a6d04e318fdda720d9f2f08f
TrebuTrey <trey.pubins@gmail.com>
2025-03-08 16:57:17 -0700

    Week 5 notes OOP

--- src/week05/oop.java
+++ src/week05/oop.java
@@ -3,7 +3,28 @@ package week05;
 public class oop {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		/*
+		 * Four Pillars of Object Oriented Programming
+		 * 
+		 * 1. Abstraction
+		 * - Learn to keep main files simple and clean.
+		 * 
+		 * 2. Encapsulation
+		 * - Functions and variables that do not need to be accessed
+		 * by other parts of the program should not be facing outward
+		 * 
+		 * 3. Inheritance
+		 * - Simplifying the dependencies and reusing structures
+		 * from parent to child classes
+		 * 
+		 * 4. Polymorphism
+		 * - Methods and functions can take different forms
+		 * 
+		 * Class is the blueprint
+		 * Object is the product of the blueprint
+		 * Object is an instance of the class
+		 *	
+		 */
 
 	}
 
