4526ec4091cd9ca6d5bce036aa435e8ff129c5d4
Trey P <121532811+TrebuTrey@users.noreply.github.com>
2025-03-08 02:42:07 -0700

    Java week 4 project and research

---
+++ src/week04/Java4Research.java
@@ -0,0 +1,19 @@
+package week04;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Java4Research {
+
+	public static void main(String[] args) {
+		List<String> stringList = new ArrayList<String>();
+		
+		Set<StringBuilder> sbSet = new HashSet<StringBuilder>();
+		
+		Map<String, String> map = new HashMap<String, String>();
+	}
+}
